8b85898c2826390ad8818294dbcc2d7b96b87d07
sebadalessandro <sebadalessandro@hotmail.com>
2022-06-22 10:13:30 -0300

    clase03 finito

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+/*immediately invoked function expression o bien por sus siglas: IIFE, 
+que es tal cual, son funciones que en cuanto se declaran, se ejecutan*/
+(()=>{
+
 const btn = document.querySelector('[data-form-btn]')
-console.log(btn)
 
 const createTask = (evento) => {
     evento.preventDefault();
@@ -9,14 +12,39 @@ const createTask = (evento) => {
     const task = document.createElement('li');
     task.classList.add('card')
     input.value = '';
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
+    const taskContent = document.createElement('div')
+    taskContent.appendChild(checkComplete())
+    const titleTask = document.createElement('span')
+    titleTask.classList.add('task')
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask)
+    const content = `
+        <i class="fas fa-trash-alt trashIcon icon"></i>`
+    //task.innerHTML = content;
+    task.appendChild(taskContent)
     list.appendChild(task)
-    console.log(content)
 }
 
 btn.addEventListener('click', createTask)
+
+const checkComplete = ()=>{
+    const i = document.createElement('i');
+    i.classList.add('far','fa-check-square','icon');
+    i.addEventListener('click',completeTask)
+    return i;
+}
+
+const completeTask = (evento)=>{
+    const element = evento.target
+    /*  
+    element.classList.remove('far')
+    element.classList.add('fas');
+    element.classList.add('completeIcon') 
+    con TOGGLE podemos verificar: si una clase no esta => se agrega... de lo contrario se quita
+    */
+    element.classList.toggle('fas');
+    element.classList.toggle('far');
+    element.classList.toggle('completeIcon') 
+}
+
+})()
